24ef79afd99b97585c3768bda82bdcfae4cc1ff5
Cheng Luyu <chengluyu@live.cn>
2016-02-25 18:00:28 +0800

    Temporary test way

---
+++ test/simple-test.js
@@ -0,0 +1,35 @@
+"use strict";
+
+let rules = [
+  {
+    "name": "identifier",
+    "pattern": "[a-zA-Z_]\\w*",
+    "ignorable": false
+  },
+  {
+    "name": "operator",
+    "pattern": "[+-\\\\*]",
+    "ignorable": false // default to false
+  },
+  {
+    "name": "integer",
+    "pattern": "-?\\d+"
+  },
+  {
+    "name": "whitespace",
+    "pattern": "\\s+",
+    "ignorable": true
+  }
+];
+
+let Tokenizer = require("../").Tokenizer, util = require("util");
+
+let tokenizer = new Tokenizer(rules);
+
+let lex = tokenizer("1+a*b-2");
+
+while (true) {
+  let obj = lex.next();
+  console.log(util.inspect(obj.value));
+  if (obj.done) break;
+}
